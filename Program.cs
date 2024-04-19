3cada715de52990b687ab69be6d5d997df56f073
devdouglasa <douglasalisson27@gmail.com>
2024-04-18 21:38:43 -0300

    Implementado testes de instancias das classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO!
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia n1 = new(numero: "1234", modelo: "Modelo 1", imei: "123422225", memoria: 128);
+n1.Ligar();
+n1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone ip = new(numero: "4562", modelo: "Modelo 2", imei: "123918249", memoria: 242);
+ip.ReceberLigacao();
+ip.InstalarAplicativo("Whatsapp");
+ip.Ligar();
